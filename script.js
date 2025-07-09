bc89e6dd533cc84776d5224193ace562c421e838
aaman <aneela.aman.is@gmail.com>
2025-07-09 15:30:26 -0400

    remove redundant console code

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const tileRow3 = document.getElementsByClassName("row3");
 let player1 = document.getElementById("player1");
 let player2 = document.getElementById("player2");
 let startButton = document.getElementById("start");
-
+let message = document.getElementById("message");
 // let score_board = document.getElementsByClassName("scoreboard");
 
 player1.addEventListener("click", player1_name);
@@ -55,8 +55,6 @@ const game = (function () {
         currentPlayer: players.X,
         gameOver: false,
     };
-    // console.log(gameBoard.htmlTiles);
-
 
     displayBoard = () => {
         for (let r = 0; r < 3; r++) {
@@ -65,19 +63,13 @@ const game = (function () {
             }
         }
 
-        // console.log(`
-        //     ${gameBoard.htmlTiles[0][0]}  | ${logicalBoard[0][1]} | ${logicalBoard[0][2]}     (0) (1) (2)
-        //     ---+---+---
-        //     ${logicalBoard[1][0]}  | ${logicalBoard[1][1]} | ${logicalBoard[1][2]}     (0) (1) (2)
-        //     ---+---+---
-        //     $logicalBoard[2][0]}  | ${logicalBoard[2][1]} | ${logicalBoard[2][2]}
-        //     `);
+      
     };
 
     const checkWin = () => {
         for (let i = 0; i < 3; i++) {
             if (logicalBoard[i][0] === gameBoard.currentPlayer && logicalBoard[i][1] === gameBoard.currentPlayer && logicalBoard[i][2] === gameBoard.currentPlayer &&
-            gameBoard.currentPlayer != players.EMPTY
+                gameBoard.currentPlayer != players.EMPTY
             ) {
                 return true;
             }
@@ -85,15 +77,15 @@ const game = (function () {
 
         for (let j = 0; j < 3; j++) {
             if (logicalBoard[0][j] === gameBoard.currentPlayer && logicalBoard[1][j] === gameBoard.currentPlayer && logicalBoard[2][j] === gameBoard.currentPlayer &&
-            gameBoard.currentPlayer != players.EMPTY 
+                gameBoard.currentPlayer != players.EMPTY
             ) {
                 return true;
             }
         }
 
         if (
-            (logicalBoard[0][0] === gameBoard.currentPlayer && logicalBoard[1][1] === gameBoard.currentPlayer && logicalBoard[2][2] === gameBoard.currentPlayer) ||
-            (logicalBoard[0[2] === gameBoard.currentPlayer && logicalBoard[1][1] === gameBoard.currentPlayer && logicalBoard[2][0] === gameBoard.currentPlayer)
+            (logicalBoard[0][0] === gameBoard.currentPlayer && logicalBoard[1][1] === gameBoard.currentPlayer && logicalBoard[2][2] === gameBoard.currentPlayer && gameBoard.currentPlayer != players.EMPTY) ||
+            (logicalBoard[0][2] === gameBoard.currentPlayer && logicalBoard[1][1] === gameBoard.currentPlayer && logicalBoard[2][0] === gameBoard.currentPlayer && gameBoard.currentPlayer != players.EMPTY)
         ) {
             return true;
         }
@@ -103,7 +95,7 @@ const game = (function () {
 
     const checkDraw = () => {
         for (let i = 0; i < 3; i++) {
-            for (j = 0; j < 3; J++) {
+            for (let j = 0; j < 3; j++) {
 
                 if (logicalBoard[i][j] === players.EMPTY) {
                     return false;
@@ -117,15 +109,12 @@ const game = (function () {
         gameBoard.currentPlayer = (gameBoard.currentPlayer === players.X) ? players.O : players.X;
     };
 
-
-    startButton.addEventListener("click", resetGame);
-
     function resetGame() {
         logicalBoard = [
             [players.EMPTY, players.EMPTY, players.EMPTY,],
             [players.EMPTY, players.EMPTY, players.EMPTY,],
             [players.EMPTY, players.EMPTY, players.EMPTY,],
-        ] ;
+        ];
 
         gameBoard.currentPlayer = players.X;
         gameBoard.gameOver = false;
@@ -136,7 +125,7 @@ const game = (function () {
     function tileListeners() {
         for (let r = 0; r < 3; r++) {
             for (let c = 0; c < 3; c++) {
-                (function(row, col) {
+                (function (row, col) {
                     gameBoard.htmlTiles[row][col].addEventListener('click', () => makeYourMove(row, col));
                 })(r, c);
             }
@@ -154,12 +143,12 @@ const game = (function () {
             return;
         }
 
-        if (gameBoard.htmlTiles[row][col] !== ' ') {
+        if (logicalBoard[row][col] !== players.EMPTY) {
             console.log(`Spot (${row}, ${col} ) is already taken. Please choose an empty spot.`);
             return;
         }
 
-        gameBoard.htmlTiles[row][col] = gameBoard.currentPlayer;
+        logicalBoard[row][col] = gameBoard.currentPlayer;
         displayBoard();
 
         if (checkWin()) {
@@ -170,21 +159,25 @@ const game = (function () {
             gameBoard.gameOver = true;
         } else {
             switchPlayer();
-            console.log(`It's Player ${gameBoard.currentPlayer}'s turn. Use game.makeYourMove(row, col)`);
+            console.log(`It's Player ${gameBoard.currentPlayer}'s turn`);
         }
-    }
+    };
 
     function startGame() {
         resetGame();
-        displayBoard();
-        console.log(`Game started! Player ${gameBoard.currentPlayer}'s turn. Use game.makeYourMove(row, col)`);
-        console.log("Example: game.makeYourMove(0, 0)");
+        tileListeners();
+        // displayBoard();
+        console.log(`Game started! Player ${gameBoard.currentPlayer}'s turn.`);
     }
+
+    startButton.addEventListener("click", startGame);
+    startButton.addEventListener("click", player1_name);
+    startButton.addEventListener("click", player2_name);
 
 
     return {
         startGame: startGame,
-        makeYourMove: makeYourMove,
+        // makeYourMove: makeYourMove,
 
     };
 
